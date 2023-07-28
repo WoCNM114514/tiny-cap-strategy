8c06d74584f8b21c9743b95419917edf7e2d40ad
Partial Distemper Enjector <133895046+WoCNM114514@users.noreply.github.com>
2023-07-28 14:16:06 +0800

    Update 小市值+成长股.py

--- 小市值+成长股.py
+++ 小市值+成长股.py
@@ -93,13 +93,14 @@ def get_stock_list(context):
     df = pd.DataFrame(index=initial_list)
     for factor in factor_list:
         df[factor] = factor_values[factor].iloc[0]
-
+        
+    # 此处复合增长率的线性组合系数可以自行调整
     df['total_score'] = 0.1 * df['operating_revenue_growth_rate'] + 0.35 * df['total_profit_growth_rate'] + 0.15 * df[
         'net_profit_growth_rate'] + 0.4 * df['earnings_growth']
     ms_list = df.sort_values(by=['total_score'], ascending=False).index[:int(0.1 * len(df))].tolist()
     ms_list = sorted_by_circulating_market_cap(ms_list)
 
-    # 3: PEG，升序前20%\TURNOVER_VOLATILITY，升序前50%；再按流通市值升序，取前5名
+    # 3: (PEG，升序前20%)/TURNOVER_VOLATILITY，升序前50%；再按流通市值升序，取前5名
     peg_list = get_single_factor_list(context, initial_list, 'PEG', True, 0, 0.2)
     peg_list = get_single_factor_list(context, peg_list, 'turnover_volatility', True, 0, 0.5)
     peg_list = sorted_by_circulating_market_cap(peg_list)
